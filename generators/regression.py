b16ce037b2b5b287aa2b661a3a89e16d9671cb98
W-Sikora <242612@student.pwr.edu.pl>
2022-05-25 21:50:30 +0200

    added regression problem generator

--- generators/regression.py
+++ generators/regression.py
@@ -1,2 +1,26 @@
-def generate_regression_problem():
-    pass
+from pandas import DataFrame
+
+import utils.assertions as assertions
+
+
+def generate_regression_problem(features: DataFrame, target_function: str, noise: DataFrame = None,
+                                shuffle: bool = True):
+    """
+    :param features:
+    :param target_function:
+    :param noise:
+    :param shuffle:
+    :return:
+    """
+    assertions.has_length(target_function)
+
+    outputs = features.eval(target_function).iloc[:, -1]
+
+    if noise is not None:
+        outputs += noise
+
+    if shuffle:
+        features = features.sample(frac=1).reset_index(drop=True)
+        outputs = outputs.sample(frac=1).reset_index(drop=True)
+
+    return features, outputs
