0b93e37e7c25eda8e2cbfe3ddce1a2351db9d196
W-Sikora <242612@student.pwr.edu.pl>
2022-05-25 21:52:18 +0200

    added available functions

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,8 +1,6 @@
-from .dataset_loader import load_bml, load_mcr, load_covid, load_apartments
+from datasets.loader import load_covid, load_apartments
 
 __all__ = [
-    'load_bml',
-    'load_mcr',
     'load_covid',
     'load_apartments'
 ]
